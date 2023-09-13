62a208608db0f9b04d45a236d897edb71d1dec47
unknown <vuminhhieu22092001@gmail.com>
2023-09-13 15:36:00 +0700

    baitap

---
+++ Baitap1.java
@@ -0,0 +1,7 @@
+public class Baitap1 {
+    public static void main(String[] args) {
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine();
+    }
+    System.out.println("Hello: "+ name)
+}
